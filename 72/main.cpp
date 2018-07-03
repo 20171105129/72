ac01ae49191b807589d98c982502da38f59ff19e
20171105129 <3038914421@qq.com>
2018-07-03 15:17:27 +0800

    Initial Commit

---
+++ 72/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  72
+//
+//  Created by yinzichao on 18/7/3.
+//  Copyright © 2018年 yinzichao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
